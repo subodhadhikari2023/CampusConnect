1db601db588a4d84a8fbf647bf5b3eba919d71b5
=subodh adhikari <=subodhadhikari2023@outlook.com>
2024-07-29 00:01:07 +0530

    Added the login functionality

--- Project/src/main/java/com/bitsunisage/campusconnect/project/controller/MyController.java
+++ Project/src/main/java/com/bitsunisage/campusconnect/project/controller/MyController.java
@@ -1,6 +1,7 @@
 package com.bitsunisage.campusconnect.project.controller;
 
 
+import com.bitsunisage.campusconnect.project.entity.student.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,17 +10,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MyController {
 
-//    @GetMapping("/login")
-//    public String hello(Model model) {
-//        Student student = new Student();
-//        model.addAttribute("student",student);
-//
-//        return "index";
-//    }
-@GetMapping("/hello")
+    @GetMapping("/hello")
+    public String hello2(Model model) {
+        Student student = new Student();
+        model.addAttribute("student",student);
+
+        return "index";
+    }
+@GetMapping("/login")
 public String hello(Model model){
-//    Student student = new Student();
-//    model.addAttribute("student",student);
-    return "home";
+    Student student = new Student();
+    model.addAttribute("student",student);
+    return "/loginfile";
 }
 }
--- Project/src/main/java/com/bitsunisage/campusconnect/project/securityConfig/SecurityConfiguration.java
+++ Project/src/main/java/com/bitsunisage/campusconnect/project/securityConfig/SecurityConfiguration.java
@@ -4,20 +4,22 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfiguration {
-    @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {
-        UserDetails subodh = User.builder().username("Subodh").password("{noop}test123").roles("STUDENT").build();
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsManager() {
+//        UserDetails subodh = User.builder().username("Subodh").password("{noop}test123").roles("STUDENT").build();
+//
+//        return new InMemoryUserDetailsManager(subodh);
+//
+//    }
 
-        return new InMemoryUserDetailsManager(subodh);
-
-    }
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.authorizeHttpRequests(configurer -> configurer.
@@ -25,7 +27,7 @@ public class SecurityConfiguration {
                         .authenticated()
                 )
                 .formLogin(form ->
-                                form.loginPage("/hello")
+                                form.loginPage("/login")
                                         .loginProcessingUrl("/authenticateTheUser").permitAll()
 
 //                ).logout(logout -> logout.permitAll()
@@ -33,5 +35,18 @@ public class SecurityConfiguration {
                 );
 
         return httpSecurity.build();
+    }
+
+    // Add support for JDBC ... no more hard coded users
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+// Define query to fetch user from database for custom tables
+        jdbcUserDetailsManager.setUsersByUsernameQuery(" select user_id, pw, active from members where user_id=?");
+//Define query to fetch roles by username
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery("select user_id, role from roles where user_id=?");
+
+
+        return jdbcUserDetailsManager;
     }
 }
