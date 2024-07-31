d7623a5e852f30c56a52cae162d36f6531247ba5
=subodh adhikari <=subodhadhikari2023@outlook.com>
2024-07-31 20:31:20 +0530

    Added the role based login and role based home screen fixes #6

--- Project/src/main/java/com/bitsunisage/campusconnect/project/securityConfig/SecurityConfiguration.java
+++ Project/src/main/java/com/bitsunisage/campusconnect/project/securityConfig/SecurityConfiguration.java
@@ -2,11 +2,15 @@ package com.bitsunisage.campusconnect.project.securityConfig;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 import javax.sql.DataSource;
 
@@ -29,6 +33,7 @@ public class SecurityConfiguration {
                 .formLogin(form ->
                                 form.loginPage("/login")
                                         .loginProcessingUrl("/authenticateTheUser").permitAll()
+                                        .successHandler(authenticationSuccessHandler())
 
 //                ).logout(logout -> logout.permitAll()
                 ).logout(LogoutConfigurer::permitAll
@@ -36,14 +41,40 @@ public class SecurityConfiguration {
 
         return httpSecurity.build();
     }
+    @Bean
+    public AuthenticationSuccessHandler authenticationSuccessHandler(){
+        return new CustomAuthenticationSuccessHandler();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(configurer ->
+                configurer
+                        .requestMatchers("/student/**").hasRole("STUDENT")
+                        .requestMatchers( "/teacher/**").hasRole("TEACHER")
+
+
+        );
+//        use HTTP Basic Authentication
+        http.httpBasic(Customizer.withDefaults());
+//        Disable CSRF
+//        In general not required for stateless Rest apis that use POST,GET,PUT AND DELETE ...
+//        http.csrf(csrf -> csrf.disable()); old way to do the same thing
+        http.csrf(AbstractHttpConfigurer::disable);
+
+
+        return http.build();
+
+    }
+
 
     // Add support for JDBC ... no more hard coded users
     @Bean
     public UserDetailsManager userDetailsManager(DataSource dataSource) {
         JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
-// Define query to fetch user from database for custom tables
+        // Define query to fetch user from database for custom tables
         jdbcUserDetailsManager.setUsersByUsernameQuery(" select user_id, pw, active from members where user_id=?");
-//Define query to fetch roles by username
+        //Define query to fetch roles by username
         jdbcUserDetailsManager.setAuthoritiesByUsernameQuery("select user_id, role from roles where user_id=?");
 
 
