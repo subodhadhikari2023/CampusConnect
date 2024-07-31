0b9ba54182da5b5030db641f6cb97247a5a22880
=subodh adhikari <=subodhadhikari2023@outlook.com>
2024-07-31 15:34:50 +0530

    added the sql scripts

---
+++ Project/sql-scripts/main.sql
@@ -0,0 +1,41 @@
+create user `campusConnect`@`localhost` identified by `Frpsxwhu2001@`;
+create database if not exists `campusConnect`;
+USE `campusConnect`;
+
+DROP TABLE IF EXISTS `roles`;
+DROP TABLE IF EXISTS `members`;
+
+--
+-- Table structure for table `members`
+--
+
+CREATE TABLE `members` (
+  `user_id` varchar(50) NOT NULL,
+  `pw` char(68) NOT NULL,
+  `active` tinyint NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Inserting data for table `members`
+--
+--
+--
+--
+-- Default passwords here are: test123
+--
+
+
+
+--
+-- Table structure for table `authorities`
+--
+
+CREATE TABLE `roles` (
+  `user_id` varchar(50) NOT NULL,
+  `role` varchar(50) NOT NULL,
+  UNIQUE KEY `authorities5_idx_1` (`user_id`,`role`),
+  CONSTRAINT `authorities5_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `members` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
