22f702aa51c1395ad0114ce9e22c51936b750324
=subodh adhikari <=subodhadhikari2023@outlook.com>
2024-08-02 00:17:21 +0530

    Resolved the redirection issue with duplicate tab

--- Project/src/main/java/com/bitsunisage/campusconnect/project/controller/MyController.java
+++ Project/src/main/java/com/bitsunisage/campusconnect/project/controller/MyController.java
@@ -2,6 +2,7 @@ package com.bitsunisage.campusconnect.project.controller;
 
 
 import com.bitsunisage.campusconnect.project.entity.student.Student;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,8 +25,15 @@ public class MyController {
 
         return "indexteacher";
     }
-@GetMapping("/login")
-public String hello(Model model){
+@GetMapping("/")
+public String hello(Model model, HttpServletRequest request){
+    // Retrieve the URL prior to login from the session attribute
+    String redirectURL = (String) request.getSession().getAttribute("url_prior_login");
+    if (redirectURL != null) {
+        // If the attribute is set, redirect to the stored URL
+        return "redirect:" + redirectURL;
+    }
+    // If the attribute is not set, show the login page
 //    Student student = new Student();
 //    model.addAttribute("student",student);
     return "loginfile";
