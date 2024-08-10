f646ba139423dc9b4375545d0c040498c0f6f46d
=subodh adhikari <=subodhadhikari2023@outlook.com>
2024-08-10 09:28:15 +0530

    Refined the structure of the project and made the changes in the stylesheet links

--- Project/src/main/java/com/bitsunisage/campusconnect/project/controller/AdminContoller.java
+++ Project/src/main/java/com/bitsunisage/campusconnect/project/controller/AdminContoller.java
@@ -24,32 +24,12 @@ public class AdminContoller {
     public String getAdminPage(Model model) {
         List<User> users = userService.findAllUsers();
        List<Roles> roles = userService.findAllRoles();
-        System.out.println(users);
-        System.out.println(roles);
+
         // Add attributes to the model if needed
         model.addAttribute("users",users);
         model.addAttribute("roles",roles);
         return "admin";
     }
 
-  /*  @PostMapping("/manage-users")
-    public String manageUsers(Model model) {
-        // Implement user management logic
-        model.addAttribute("message", "User management action performed");
-        return "admin";
-    }
 
-    @PostMapping("/settings")
-    public String openSettings(Model model) {
-        // Implement settings logic
-        model.addAttribute("message", "Settings action performed");
-        return "admin";
-    }
-
-    @PostMapping("/reports")
-    public String viewReports(Model model) {
-        // Implement reports logic
-        model.addAttribute("message", "Reports action performed");
-        return "admin";
-    }*/
 }
--- Project/src/main/java/com/bitsunisage/campusconnect/project/controller/MyController.java
+++ Project/src/main/java/com/bitsunisage/campusconnect/project/controller/MyController.java
@@ -9,17 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MyController {
 
-    @GetMapping("/student")
-    public String hello2() {
-
-        return "indexstudent";
-    }
-
-    @GetMapping("/teacher")
-    public String hello3() {
-        return "indexteacher";
-    }
-
     @GetMapping("/")
     public String hello(HttpServletRequest request) {
         // Retrieve the URL prior to login from the session attribute
