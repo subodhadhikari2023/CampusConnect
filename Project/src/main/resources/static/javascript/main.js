231189794706c58f21fbcd13a72761a57283b4a5
Manav Agarwal <manav9981@outlook.com>
2024-08-25 11:47:15 +0530

    Moved the javascript to another directory

--- Project/src/main/resources/static/javascript/main.js
+++
@@ -1,8 +0,0 @@
-function toggleSidebar() {
-    document.querySelector('.sidebar').classList.toggle('collapsed');
-}
-
-
-//<form action="#" th:action="@{/logout}" method="POST" onsubmit="showLogoutMessage()">
-//<button type="submit" class="btn btn-info btn-success">Logout</button>
-//</form>
