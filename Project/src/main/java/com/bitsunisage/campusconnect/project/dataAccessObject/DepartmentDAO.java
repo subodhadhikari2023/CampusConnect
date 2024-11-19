9cf7c76a3a9fdb0fd99d17c95957f2dc12b09b70
Subodh Adhikari <subodhadhikari2023@outlook.com>
2024-11-19 14:36:29 +0530

    Created Department entity and mapped it with department table

---
+++ Project/src/main/java/com/bitsunisage/campusconnect/project/dataAccessObject/DepartmentDAO.java
@@ -0,0 +1,12 @@
+package com.bitsunisage.campusconnect.project.dataAccessObject;
+
+import com.bitsunisage.campusconnect.project.entities.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+
+public interface DepartmentDAO extends JpaRepository<Department, String> {
+//    List<Department> findAllDepartment();
+//    List<Department> findAll();
+}
