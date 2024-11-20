40ddaf632878ed5c2969aadec0ae6f7175c42f1a
Subodh Adhikari <subodhadhikari2023@outlook.com>
2024-11-20 11:21:26 +0530

    Automated backup on 2024-11-20 11:21:26

---
+++ Project/sql-scripts/databaseScript.sql
@@ -0,0 +1,141 @@
+-- MySQL dump 10.13  Distrib 8.0.40, for Linux (x86_64)
+--
+-- Host: localhost    Database: campusConnect
+-- ------------------------------------------------------
+-- Server version	8.0.40
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `department`
+--
+
+DROP TABLE IF EXISTS `department`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `department` (
+  `department_id` int NOT NULL AUTO_INCREMENT,
+  `department_name` varchar(50) NOT NULL,
+  PRIMARY KEY (`department_id`),
+  UNIQUE KEY `department_id` (`department_id`),
+  UNIQUE KEY `department_name` (`department_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=1004 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `department`
+--
+
+LOCK TABLES `department` WRITE;
+/*!40000 ALTER TABLE `department` DISABLE KEYS */;
+INSERT INTO `department` VALUES (1001,'Computer Applications'),(1002,'Computer Science'),(1003,'Department of Physics');
+/*!40000 ALTER TABLE `department` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `department_details`
+--
+
+DROP TABLE IF EXISTS `department_details`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `department_details` (
+  `department_member_id` int NOT NULL AUTO_INCREMENT,
+  `department_id` int NOT NULL,
+  `user_name` varchar(50) NOT NULL,
+  `role` varchar(50) NOT NULL,
+  PRIMARY KEY (`department_member_id`),
+  UNIQUE KEY `department_member_id` (`department_member_id`),
+  UNIQUE KEY `user_name` (`user_name`),
+  UNIQUE KEY `user_name_2` (`user_name`,`department_id`),
+  KEY `department_id` (`department_id`),
+  CONSTRAINT `department_details_ibfk_1` FOREIGN KEY (`department_id`) REFERENCES `department` (`department_id`),
+  CONSTRAINT `department_details_ibfk_2` FOREIGN KEY (`user_name`) REFERENCES `members` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2003 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `department_details`
+--
+
+LOCK TABLES `department_details` WRITE;
+/*!40000 ALTER TABLE `department_details` DISABLE KEYS */;
+INSERT INTO `department_details` VALUES (2001,1001,'Prerna','HOD'),(2002,1002,'Subodh','STUDENT');
+/*!40000 ALTER TABLE `department_details` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `members`
+--
+
+DROP TABLE IF EXISTS `members`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `members` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` varchar(50) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `pw` char(68) NOT NULL,
+  `active` tinyint NOT NULL,
+  `department` varchar(100) NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `id` (`id`),
+  UNIQUE KEY `user_id` (`user_id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `members`
+--
+
+LOCK TABLES `members` WRITE;
+/*!40000 ALTER TABLE `members` DISABLE KEYS */;
+INSERT INTO `members` VALUES (2,'Ganesh','Ganesh@outlook.com','{noop}test123',1,'Default Value'),(6,'hgdf','a@b.c','{noop}wertf',0,'Computer Applications'),(5,'Kamlesh','kam@gmail.com','{noop}ytijjghv b',0,'Computer Science'),(4,'Prerna','prerna@outlook.com','{noop}test123',1,'Default Value'),(7,'sderf','er@f.v','{noop}dsg',0,'Computer Science'),(3,'Shristi','Shristi@outlook.com','{noop}test123',1,'Default Value'),(1,'Subodh','subodhadhikari2023@outlook.com','{noop}test123',1,'Default Value');
+/*!40000 ALTER TABLE `members` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `roles`
+--
+
+DROP TABLE IF EXISTS `roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `roles` (
+  `user_id` varchar(50) NOT NULL,
+  `role` varchar(50) NOT NULL,
+  KEY `authorities5_ibfk_1` (`user_id`),
+  CONSTRAINT `authorities5_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `members` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `roles`
+--
+
+LOCK TABLES `roles` WRITE;
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+INSERT INTO `roles` VALUES ('Ganesh','ROLE_ADMIN'),('Shristi','ROLE_TEACHER'),('Prerna','ROLE_HOD'),('Kamlesh','ROLE_TEACHER'),('hgdf','ROLE_STUDENT'),('sderf','ROLE_HOD');
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-11-20 11:19:48
