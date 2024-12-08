d397666aeea84d9801b69b9f7bfe06e2fc8b0bdc
Manav Agarwal <manav9981@outlook.com>
2024-12-08 14:57:09 +0530

    Form uploaded for both teacher and student for the sharing of resources

--- Project/src/main/java/com/bitsunisage/campusconnect/project/controller/StudentController.java
+++ Project/src/main/java/com/bitsunisage/campusconnect/project/controller/StudentController.java
@@ -10,4 +10,35 @@ public class StudentController {
 
         return "studentViewPages/indexstudent";
     }
+    @GetMapping("/PPTs")
+    public String PPTs() {
+
+        return "studentViewPages/searchppts";
+    }
+    @GetMapping("/notes")
+    public String notes() {
+
+        return "studentViewPages/searchnotes";
+    }
+    @GetMapping("/programs")
+    public String programs() {
+
+        return "studentViewPages/searchprograms";
+    }
+    @GetMapping("/audiobooks")
+    public String audiobooks() {
+
+        return "studentViewPages/searchaudiobooks";
+    }
+    @GetMapping("/referencebooks")
+    public String reference() {
+
+        return "studentViewPages/searchreferencebooks";
+    }
+    @GetMapping("/video")
+    public String video() {
+
+        return "studentViewPages/searchvideos";
+    }
+
 }
