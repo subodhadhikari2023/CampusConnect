8957f41e9b4c1985323ed0fbfa5907e0c9f1614e
Subodh Adhikari <subodhadhikari2023@outlook.com>
2024-12-12 15:10:32 +0530

    Added the download options for different compression techniques

--- Project/src/main/java/com/bitsunisage/campusconnect/project/controller/StudentController.java
+++ Project/src/main/java/com/bitsunisage/campusconnect/project/controller/StudentController.java
@@ -4,13 +4,23 @@ import com.bitsunisage.campusconnect.project.DataTransferObject.FileUploadDTO;
 import com.bitsunisage.campusconnect.project.entities.*;
 import com.bitsunisage.campusconnect.project.service.StorageService;
 import com.bitsunisage.campusconnect.project.service.UserService;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.io.*;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class StudentController {
@@ -30,7 +40,7 @@ public class StudentController {
         List<CourseDetails> courseDetailsList = userService.getAllCourses();
         List<Semester> semesterList = userService.getAllSemesters();
         List<SubjectDetails> subjectDetailsList = userService.getAllSubjects();
-        model.addAttribute("userList",userList);
+        model.addAttribute("userList", userList);
         model.addAttribute("departments", departmentsList);
         model.addAttribute("courses", courseDetailsList);
         model.addAttribute("semesterList", semesterList);
@@ -46,7 +56,7 @@ public class StudentController {
     @GetMapping("student/PPTs")
     public String PPTs(Model model) {
         FileUploadDTO fileUploadDTO = new FileUploadDTO();
-        fileUploadDTO.setFileRole("Notes");
+        fileUploadDTO.setFileRole("PPTs");
         formModelFeeding(model);
         model.addAttribute("fileUploadDTO", fileUploadDTO);
 
@@ -83,7 +93,7 @@ public class StudentController {
     @GetMapping("/student/referencebooks")
     public String reference(Model model) {
         FileUploadDTO fileUploadDTO = new FileUploadDTO();
-        fileUploadDTO.setFileRole("ReferenceBooks");
+        fileUploadDTO.setFileRole("ReferenceBook");
         formModelFeeding(model);
         model.addAttribute("fileUploadDTO", fileUploadDTO);
         return "studentViewPages/searchppts";
@@ -118,4 +128,77 @@ public class StudentController {
         System.out.println(fileDataList);
         return "studentViewPages/PPTs";
     }
+
+
+
+    @GetMapping("/student/download/original/{fileId}")
+    public ResponseEntity<Resource> downloadOriginalFile(@PathVariable Long fileId) {
+        Optional<FileData> fileData = storageService.getFileById(fileId);
+        File file = new File(fileData.get().getFilePath(), fileData.get().getFileName());
+
+        if (!file.exists()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+
+        Resource resource = new FileSystemResource(file);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileData.get().getFileName() + "\"");
+
+        return ResponseEntity.ok()
+                .headers(headers)
+                .contentLength(file.length())
+                .contentType(MediaType.APPLICATION_OCTET_STREAM) // or specify the file type, e.g., MediaType.APPLICATION_PPTX
+                .body(resource);
+    }
+
+    @GetMapping("/student/download/gzip/{fileId}")
+    public ResponseEntity<Resource> downloadGzipFile(@PathVariable Long fileId) {
+        Optional<FileData> fileData = storageService.getFileById(fileId);
+        File file = new File(fileData.get().getFilePath(), fileData.get().getFileName());
+
+        if (!file.exists()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+
+        try {
+            InputStream compressedFileStream = storageService.compressFileWithGzip(file.getAbsolutePath());
+            ByteArrayResource resource = new ByteArrayResource(compressedFileStream.readAllBytes());
+            HttpHeaders headers = new HttpHeaders();
+            headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileData.get().getFileName() + ".gz\"");
+            headers.add(HttpHeaders.CONTENT_TYPE, "application/gzip");
+
+            return ResponseEntity.ok()
+                    .headers(headers)
+                    .contentLength(resource.contentLength())
+                    .body(resource);
+        } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+    }
+
+    @GetMapping("/student/download/zip/{fileId}")
+    public ResponseEntity<Resource> downloadZipFile(@PathVariable Long fileId) {
+        Optional<FileData> fileData = storageService.getFileById(fileId);
+        File file = new File(fileData.get().getFilePath(), fileData.get().getFileName());
+
+        if (!file.exists()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+
+        try {
+            InputStream zipFileStream = storageService.compressFileWithZip(file.getAbsolutePath());
+            ByteArrayResource resource = new ByteArrayResource(zipFileStream.readAllBytes());
+            HttpHeaders headers = new HttpHeaders();
+            headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileData.get().getFileName() + ".zip\"");
+            headers.add(HttpHeaders.CONTENT_TYPE, "application/zip");
+
+            return ResponseEntity.ok()
+                    .headers(headers)
+                    .contentLength(resource.contentLength())
+                    .body(resource);
+        } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+    }
+
 }
